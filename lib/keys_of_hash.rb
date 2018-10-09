ea683560e866dc7f18c463568a645c96d3f7b4d7
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-09 21:16:31 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(hash, *values)
+    ans = []
+    values.each do |each_value|
+      hash.each do |key, value|
+        if key[value] == each_value
+          ans.push(key)
+        end
+      end
+    end
+  end
+      ans
   end
 end
